e572f437613f72a10b2aad021c7341de174af130
Hamikadze <best.homa@gmail.com>
2021-02-14 01:01:35 +0500

    Remove cursor, add letter highlight, fix TextArea text loading

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -2,30 +2,24 @@ import React, {useEffect, useLayoutEffect, useRef, useState} from "react";
 import './TextArea.css';
 
 function TextArea(props) {
-    const [cursorState, setCursorState] = useState(false);
-    const [cursorRed, setCursorRed] = useState(false);
     const [printedText, setPrintedText] = useState([]);
-    const [text, setText] = useState([]);
+    const [text, setText] = useState(props.text);
+    const [typo, setTypo] = useState(false);
     const textAreaBox = useRef(null);
+
 
     const handleKeyDown = (event) => {
         if (event.altKey || event.ctrlKey || (event.shiftKey && event.keyCode === 16))
             return;
-        console.log([printedText, text, props.text])
         if (text[0] === event.key) {
+            setTypo(false);
             setText(v => v.filter((i, index) => index !== 0));
             setPrintedText(v => [...v, event.key]);
-            setCursorRed(false);
         } else {
-            setCursorRed(true);
-            setCursorState(true);
-            setTimeout(() => {
-                setCursorRed(false);
-            }, 1000);
-            event.preventDefault();
+            setTypo(true);
         }
+        event.preventDefault();
     }
-
 
     useEffect(() => {
         if (textAreaBox && textAreaBox.current) {
@@ -39,31 +33,10 @@ function TextArea(props) {
         }
     }, [printedText])
 
-    useEffect(() => {
-        setText(props.text);
-
-        console.log([printedText, text, props.text])
-    }, [props])
-
-    useEffect(() => {
-        const id = setInterval(() => {
-            if (textAreaBox.current === document.activeElement) {
-                setCursorState(v => !v);
-            } else {
-                setCursorState(false);
-            }
-        }, 500);
-        return () => {
-            clearInterval(id);
-            setCursorState(false);
-        };
-    }, [])
-
 
     return (<div ref={textAreaBox} className={'TextArea'} tabIndex="0">
-        {printedText.map((ch,i) => <span key={`printed-${i}`} className={'printed-text'}>{ch}</span>)}
-        <span className={`cursor ${cursorRed ? 'cursor-red' : ''} ${cursorState ? "" : "hidden"}`}>&nbsp;</span>
-        {text.map((ch, i) => <span className={'wait-text'} key={`text-${i}`}>{ch}</span>)}
+        {printedText.map((ch, i) => <span className={'printed-text'} key={`printed-${i}`}>{ch}</span>)}
+        {text.map((ch, i) => <span className={`wait-text ${i === 0 ? (typo ? 'ch-red' : 'ch-green'): ''}`} key={`text-${i}`}>{ch}</span>)}
     </div>)
 }
 
